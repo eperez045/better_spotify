a3f8bbd77ab20fc79df725674c8346ac4357b1fb
dmarcela2022 <104063341+dmarcela2022@users.noreply.github.com>
2022-08-22 14:17:19 +0200

    auth-dashboard

--- client/src/pages/Tab1.tsx
+++ client/src/pages/Tab1.tsx
@@ -2,25 +2,30 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/rea
 // import ExploreContainer from '../components/ExploreContainer';
 import Login from '../components/Login';
 import './Tab1.css';
+import Dashboard from '../components/Dashboard';
 
+const code = new URLSearchParams(window.location.search).get('code')
 const Tab1: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <Login />
-      </IonContent>
-    </IonPage>
+    // <IonPage>
+    //   <IonHeader>
+    //     <IonToolbar>
+    //       <IonTitle>Tab 1</IonTitle>
+    //     </IonToolbar>
+    //   </IonHeader>
+    //   <IonContent fullscreen>
+    //     <IonHeader collapse="condense">
+    //       <IonToolbar>
+    //         <IonTitle size="large">Tab 1</IonTitle>
+    //       </IonToolbar>
+    //     </IonHeader>
+    //     <Login />
+    //   </IonContent>
+    // </IonPage>
+    code ? <Dashboard code={code} /> : <Login />
+
   );
+
 };
 
 export default Tab1;
